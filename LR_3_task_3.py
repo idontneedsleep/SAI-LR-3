846e8d65f4ea63d5708df1edef9fb1526ece99a6
Евеліна Федоренко <ipz192_feo@student.ztu.edu.ua>
2023-05-18 19:28:32 +0300

    LR_3_task_3

--- LR_3_task_3.py
+++ LR_3_task_3.py
@@ -1,30 +1,23 @@
-import pickle
+from math import degrees
 import numpy as np
 from sklearn import linear_model
 import sklearn.metrics as sm
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
 
-input_file = 'data_regr_4.txt'
-
+input_file = 'data_multivar_regr.txt'
 data = np.loadtxt(input_file, delimiter=',')
+
 X, y = data[:, :-1], data[:, -1]
 
 num_training = int(0.8 * len(X))
 num_test = len(X) - num_training
-
 X_train, y_train = X[:num_training], y[:num_training]
 X_test, y_test = X[num_training:], y[num_training:]
 
-regressor = linear_model.LinearRegression()
-regressor.fit(X_train, y_train)
+linear_regressor = linear_model.LinearRegression()
+linear_regressor.fit(X_train, y_train)
 
-y_test_pred = regressor.predict(X_test)
-
-plt.scatter(X_test, y_test, color='green')
-plt.plot(X_test, y_test_pred, color='black', linewidth=4)
-plt.xticks(())
-plt.yticks(())
-plt.show()
+y_test_pred = linear_regressor.predict(X_test)
 
 print("Linear regressor performance:")
 print("Mean absolute error =",
@@ -37,12 +30,15 @@ print("Explain variance score =",
       round(sm.explained_variance_score(y_test, y_test_pred), 2))
 print("R2 score =", round(sm.r2_score(y_test, y_test_pred), 2))
 
-output_model_file = 'model2.pkl'
-with open(output_model_file, 'wb') as f:
-    pickle.dump(regressor, f)
+polynomial = PolynomialFeatures(degree=10)
+X_train_transformed = polynomial.fit_transform(X_train)
 
-with open(output_model_file, 'rb') as f:
-    regressor_model = pickle.load(f)
-y_test_pred_new = regressor_model.predict(X_test)
-print("\nNew mean absolute error =",
-      round(sm.mean_absolute_error(y_test, y_test_pred_new), 2))
+datapoint = [[7.75, 6.35, 5.56]]
+poly_datapoint = polynomial.fit_transform(datapoint)
+
+poly_linear_model = linear_model.LinearRegression()
+poly_linear_model.fit(X_train_transformed, y_train)
+print("\nLinear regression:\n",
+      linear_regressor.predict(datapoint))
+print("\nPolynomial regression:\n",
+      poly_linear_model.predict(poly_datapoint))
